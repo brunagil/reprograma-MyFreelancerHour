5bdcdd52daf0b41a2f2eb326a641713ffbe0a171
Bruna Roberta Gil <brunagil.eppen@gmail.com>
2018-11-24 19:45:20 -0200

    update css and img files; create my app w/ functional components and functions

---
+++ src/components/fieldset/fieldSet.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FieldSet = (props) => {
+    return (
+        <fieldset>
+            <legend>{props.legend}</legend>
+            <div className="secao__formulario__grupo">
+                <label htmlFor={props.idInput}>{props.label}</label>
+                <input id={props.idInput} type={props.type} value={props.value} onChange={props.onChange} />
+            </div>
+    </fieldset>
+    )
+}
+
+export default FieldSet
